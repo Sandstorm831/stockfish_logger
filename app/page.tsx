95a8510b87d19cd7da210f53acc4af5f13cdefee
rohan <gargrohan138@gmail.com>
2025-01-14 19:33:37 +0530

    configuring xmlhttprequest for fetching stockfish.wasm

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,58 @@
-import Image from "next/image";
+'use client'
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const formatMB = (n) => {
+  return (n ? (n / 1e6).toPrecision(3) : "?") + "MB";
+};
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [value, setValue] = useState("");
+  useEffect(()=>{
+    const script = document.createElement("script");
+    script.src = "/lib/stockfish.js";
+    script.async = true;
+    document.body.appendChild(script);
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', '/lib/stockfish.wasm', true);
+    xhr.setRequestHeader('Accept', '*/*');
+    xhr.setRequestHeader('x-decompressed-content-length', '7031229');
+    xhr.responseType = 'arraybuffer';
+    xhr.onloadend = (doneEvent) => {
+      console.log(`decoded length of the response : ${doneEvent.loaded}`)
+    }
+    xhr.onprogress = (ProgressEvent) => {
+      let loaded = ProgressEvent.loaded;
+      let total = ProgressEvent.total || 7031229;
+      console.log(`loaded : ${formatMB(loaded)} / ${formatMB(total)}`)
+    }
+    script.onload = () => {
+      xhr.send();
+    }
+    return() => {
+      document.body.removeChild(script);
+    }
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  }, [])
+  return (
+    <div className="w-screen h-screen flex flex-col">
+      <div className="flex pt-5 pl-5 w-full">
+        <input type="string" placeholder="Enter UCI Command Here" className="w-5/6 border px-2" onChange={(e) => setValue(e.target.value)} value={value} />
+        <button className="bg-[#888888] text-white px-2 mr-5">SEND</button>
+        <select className="px-3">
+          <option >-- EXAMPLE --</option>
+          <option value={"stop"} onClick={() => setValue("stop")}>stop</option>
+          <option value={"uci"} onClick={() => setValue("uci")}>uci</option>
+          <option value={"go depth 15"} onClick={() => setValue("go depth 15")}>go depth 15</option>
+          <option value={"go infinite"} onClick={() => setValue("go infinite")}>go infinite</option>
+        </select>
+      </div>
+        <div className="pl-5 w-full mt-3">- download : </div>
+        <div className="pl-5 w-full mb-3">- stockfish state : </div>
+        <div className="mx-5 border flex-1 overflow-scroll">
+          
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
